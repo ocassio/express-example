6e4eb2dddb500dc20a3212c5a58284a2f9a247ce
alio0414 <aleksandr.ionov@netcracker.com>
2019-11-21 15:36:49 +0400

    Add REST API

--- src/repositories/users.repository.js
+++ src/repositories/users.repository.js
@@ -1,16 +1,31 @@
 const path = require('path');
+const uuid = require('uuid/v1');
 const { readJsonFile, writeJsonFile } = require('../utils/file.utils');
 
 const FILE_PATH = path.resolve(path.dirname(require.main.filename), '..', 'data', 'users.json');
 
 const usersRepository = {
+
     async getAll() {
         return await readJsonFile(FILE_PATH) || [];
     },
+
+    async get(id) {
+        const users = await this.getAll();
+        return users.find(user => user.id === id);
+    },
+
     async add(user) {
         const users = await this.getAll();
-        users.push(user);
+        const newUser = {
+            id: uuid(),
+            ...user
+        };
+        users.push(newUser);
+
         await writeJsonFile(FILE_PATH, users);
+
+        return newUser;
     }
 };
 
